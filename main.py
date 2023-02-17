eb0fbb922b228a8bba710b24d83ac81c511519bf
DEOXCE <deoxceofficial@gmail.com>
2023-02-17 23:59:22 +0500

    improved code readability

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ import re
 
 intents = discord.Intents.default()
 intents.message_content = True
-activity = discord.Game(name = "server offline")
-client = commands.Bot(intents = intents, activity = activity, status = discord.Status.idle)
+activity = discord.Game(name="server offline")
+client = commands.Bot(intents=intents, activity=activity, status=discord.Status.idle)
 started = False
-last_panel:discord.Interaction = None
+panel: discord.Interaction = None
 
 @client.event
 async def on_ready():
     print("ready")
 
-@client.slash_command(name = "server", description = "server control panel", guild = discord.Object(id = config.guild_id))
+@client.slash_command(name="server", description="server control panel", guild=discord.Object(id=config.guild_id))
 async def server(ctx: discord.Interaction):
-    global last_panel
-    if last_panel != None:
-        await last_panel.delete_original_response()
+    global panel
+    if panel is not None:
+        await panel.delete_original_response()
     view = await control_panel()
-    last_panel = await ctx.response.send_message("control panel", view=view)
+    panel = await ctx.response.send_message("control panel", view=view)
 
 async def control_panel():
     view = View()
@@ -44,94 +44,99 @@ async def control_panel():
     view.add_item(button_render)
     return view
 
-minecraft = client.create_group(name = "minecraft", guild = discord.Object(id = config.guild_id))
+minecraft = client.create_group(name="minecraft", guild=discord.Object(id=config.guild_id))
 
-@minecraft.command(name = "start", description = "start minecraft server", guild = discord.Object(id = config.guild_id))
+@minecraft.command(name="start", description="start minecraft server", guild=discord.Object(id=config.guild_id))
 async def mc_start(ctx: discord.Interaction):
     global started
     if not started:
         os.startfile(config.run_bat)
-        server = JavaServer.lookup(config.server_ip, timeout = 3)
+        server = JavaServer.lookup(config.server_ip)
         await ctx.response.send_message("server is starting")
         while not started:
             try:
                 status = server.status()
-                if status: 
+                if status:
                     started = True
             except Exception: 
                 print("starting..")
-        await ctx.followup.send("server started")
+        await ctx.channel.send("server started")
         print("server started")
-        await client.change_presence(status = discord.Status.online)
-        await client.change_presence(activity = discord.Game(name = f"server online"))
-        file = io.open(config.console, mode = "r", encoding = "utf-8")
+        await client.change_presence(status=discord.Status.online)
+        await client.change_presence(activity=discord.Game(name="server online"))
+        file = io.open(config.console, mode="r", encoding="utf-8")
         line = len(file.readlines()) - 1
         while started:
             line = await console_reader(line)
     else:
         await ctx.response.send_message("server is already running", ephemeral=True)
 
-@minecraft.command(name = "stop", description = "stop minecraft server", guild = discord.Object(id = config.guild_id))
+@minecraft.command(name="stop", description="stop minecraft server", guild=discord.Object(id=config.guild_id))
 async def mc_stop(ctx: discord.Interaction):
-    global started
-    await client.change_presence(status = discord.Status.idle)
-    await client.change_presence(activity = discord.Game(name = f"server offline"))
+    await client.change_presence(status=discord.Status.idle)
+    await client.change_presence(activity=discord.Game(name="server offline"))
     try:
         with MCRcon(config.rcon_ip, config.rcon_pass) as mcr:
-            mcr.command('stop')
+            mcr.command("stop")
             mcr.disconnect()
+            global started
             started = False
             await ctx.response.send_message("server stopped")
             print("server stopped")
     except Exception:
         await ctx.response.send_message("server is already stopped", ephemeral=True)
 
-@minecraft.command(name = "status", description = "server status", guild = discord.Object(id = 1064616038383231047))
+@minecraft.command(name="status", description="server status", guild=discord.Object(id=config.guild_id))
 async def mc_status(ctx: discord.Interaction, ip: str = config.server_ip):
     if ip.find(":") == -1:
         ip += ":25565"
-    server = JavaServer.lookup(ip, timeout = 1)
+    server = JavaServer.lookup(ip)
     try:
         status = server.status()
-        query = server.query()
-        await ctx.response.send_message(f"ip: {ip}\nversion: {status.version.name}\ndescription: {status.description}\nplayers: {status.players.online}\n{', '.join(query.players.names)}")
+        try:
+            query = server.query()
+            await ctx.response.send_message(f"ip: {ip}\nversion: {status.version.name}\ndescription: {status.description}\nplayers: {status.players.online}\n{', '.join(query.players.names)}")
+        except Exception:
+            await ctx.response.send_message(f"ip: {ip}\nversion: {status.version.name}\ndescription: {status.description}\nplayers: {status.players.online}")
     except Exception:
         await ctx.response.send_message(f"server {ip} offline")
 
-@minecraft.command(name = "render", description = "render server map", guild = discord.Object(id = 1064616038383231047))
+@minecraft.command(name="render", description="render server map", guild=discord.Object(id=config.guild_id))
 async def mc_render(ctx: discord.Interaction):
     await ctx.response.send_message("in developing")
 
 @client.event
 async def on_message(message: discord.Message):
-    global started
-    if message.author != client.user and (message.channel.id == config.chat_channel_id or message.channel.id == config.console_channel_id):
-        if len(f"<{message.author.name}> {message.content}") < 800 and len(message.content) > 0:
-            try: 
+    if message.author != client.user:
+        if message.channel.id == config.chat_channel_id:
+            if len(f"<{message.author.name}> {message.content}") < 800 and len(message.content) > 0:
                 with MCRcon(config.rcon_ip, config.rcon_pass) as mcr:
-                    if message.channel.id == config.chat_channel_id:
-                        mcr.command(f"tellraw @a [\"\",{{\"text\":\"<{message.author.name}>\",\"bold\":true,\"color\":\"#7289DA\",\"clickEvent\":{{\"action\":\"copy_to_clipboard\",\"value\":\"{message.author.name}#{message.author.discriminator}\"}},\"hoverEvent\":{{\"action\":\"show_text\",\"contents\":\"{message.author.name}#{message.author.discriminator}\"}}}},{{\"text\":\" {message.content}\"}}]")
-                    if message.channel.id == config.console_channel_id:
-                        resp = mcr.command(message.content)
-                        if resp == "Stopping the server":
-                            resp = "server stopped"
-                            started = False
-                        await message.reply(resp, mention_author = False)
-                        print("server stopped")
+                    mcr.command(f"tellraw @a [\"\",{{\"text\":\"<{message.author.name}>\",\"bold\":true,\"color\":\"#7289DA\",\"clickEvent\":{{\"action\":\"copy_to_clipboard\",\"value\":\"{message.author.name}#{message.author.discriminator}\"}},\"hoverEvent\":{{\"action\":\"show_text\",\"contents\":\"{message.author.name}#{message.author.discriminator}\"}}}},{{\"text\":\" {message.content}\"}}]")
                 mcr.disconnect()
-            except Exception:
-                pass
-        else:
-            await message.add_reaction('💀')
+            else:
+                await message.add_reaction("💀")
+        if message.channel.id == config.console_channel_id:
+            if 0 < len(message.content) < 256:
+                with MCRcon(config.rcon_ip, config.rcon_pass) as mcr:
+                    resp = mcr.command(message.content)
+                    if resp == "Stopping the server":
+                        resp = "server stopped"
+                        global started
+                        started = False
+                        print("server stopped")
+                    await message.reply(resp, mention_author=False)
+                mcr.disconnect()
+            else:
+                await message.add_reaction("💀")
 
 async def console_reader(line):
-    global started
     chat_channel = client.get_channel(config.chat_channel_id)
     console_channel = client.get_channel(config.console_channel_id)
-    file = io.open(config.console, mode = "r", encoding = "utf-8")
+    file = io.open(config.console, mode="r", encoding="utf-8")
     line_list = file.readlines()
     while line < len(line_list):
         if re.match(r"\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9] INFO\]: (.+?) issued server command: /stop", line_list[line]):
+            global started
             started = False
             await console_channel.send(line_list[line])
             message = await console_channel.fetch_message(console_channel.last_message_id)
@@ -139,7 +144,7 @@ async def console_reader(line):
             print("server stopped")
             break
         nickname = re.search(r"<(.+?)>", line_list[line])
-        if nickname != None:
+        if nickname is not None:
             await chat_channel.send(f"**{nickname.group(1)}**:{line_list[line][line_list[line].find('>') + 1:]}")
         else:
             await console_channel.send(line_list[line])

d2a8db2660149ddf80c7d6ec5aa9b21b9789f2e4
DEOXCE <deoxceofficial@gmail.com>
2023-02-26 20:46:46 +0500

    find command and some fixes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import re
 intents = discord.Intents.default()
 intents.message_content = True
 activity = discord.Game(name="server offline")
-client = commands.Bot(intents=intents, activity=activity, status=discord.Status.idle)
+client = commands.Bot(intents=intents, activity=activity)
 started = False
 start_button_state = True
 stop_button_state = False
@@ -21,19 +21,12 @@ panel_message: discord.Message = None
 console_output = False
 global_interaction: discord.Interaction = None
 stop_already_called = False
+nickname = None
+find_stop = False
 
 @client.event
 async def on_ready():
     print("ready")
-
-@client.slash_command(name="server", description="server control panel", guild=discord.Object(id=config.guild_id))
-async def server(ctx: discord.Interaction):
-    global panel, start_button_state, stop_button_state, panel_message
-    if panel is not None:
-        await panel_message.delete()
-    view = await control_panel(start_button_state, stop_button_state)
-    panel = await ctx.response.send_message("control panel", view=view)
-    panel_message = await panel.channel.fetch_message(panel.channel.last_message_id)
 
 async def control_panel(start_button_state, stop_button_state):
     view = View(timeout=None)
@@ -54,21 +47,33 @@ async def control_panel(start_button_state, stop_button_state):
     view.add_item(button_render)
     return view
 
-minecraft = client.create_group(name="minecraft", guild=discord.Object(id=config.guild_id))
+server_group = client.create_group(name="server", guild=discord.Object(id=config.guild_id))
 
-@minecraft.command(name="start", description="start minecraft server", guild=discord.Object(id=config.guild_id))
+@server_group.command(name="panel", description="server control panel", guild=discord.Object(id=config.guild_id))
+async def server(ctx: discord.Interaction):
+    global panel, start_button_state, stop_button_state, panel_message
+    if panel is not None:
+        await panel_message.delete()
+    view = await control_panel(start_button_state, stop_button_state)
+    panel = await ctx.response.send_message("control panel", view=view)
+    panel_message = await panel.original_response()
+    #panel_message = await panel.channel.fetch_message(panel.channel.last_message_id)
+    #print(panel.channel.last_message_id, panel.message.reference.message_id)
+
+@server_group.command(name="start", description="start minecraft server", guild=discord.Object(id=config.guild_id))
 async def mc_start(ctx: discord.Interaction):
     global started, panel, start_button_state, stop_button_state, panel_message, console_output
     if not started:
         start_button_state = False
         os.startfile(config.run_bat)
-        server = JavaServer.lookup(f"{config.server_ip}:{config.server_port}")
+        server = JavaServer.lookup(f"{config.server_ip}:{config.server_port}", timeout=0.05)
         if panel is not None:
             view = await control_panel(start_button_state, stop_button_state)
             await panel_message.edit(view=view)
         print("server is starting")
         await ctx.response.defer(ephemeral=False, invisible=False)
         while not started:
+            await asyncio.sleep(0)
             try:
                 status = server.status()
                 if status:
@@ -81,7 +86,6 @@ async def mc_start(ctx: discord.Interaction):
         if panel is not None:
             view = await control_panel(start_button_state, stop_button_state)
             await panel_message.edit(view=view)
-        await client.change_presence(status=discord.Status.online)
         await client.change_presence(activity=discord.Game(name="server online"))
         file = io.open(config.console, mode="r", encoding="utf-8")
         line = len(file.readlines()) - 1
@@ -91,10 +95,9 @@ async def mc_start(ctx: discord.Interaction):
     else:
         await ctx.response.send_message("server is already running", ephemeral=True)
 
-@minecraft.command(name="stop", description="stop minecraft server", guild=discord.Object(id=config.guild_id))
+@server_group.command(name="stop", description="stop minecraft server", guild=discord.Object(id=config.guild_id))
 async def mc_stop(ctx: discord.Interaction):
     global stop_already_called
-    await client.change_presence(status=discord.Status.idle)
     await client.change_presence(activity=discord.Game(name="server offline"))
     if not stop_already_called:
         try:
@@ -114,7 +117,7 @@ async def mc_stop(ctx: discord.Interaction):
         except Exception:
             await ctx.response.send_message("server is already stopped", ephemeral=True)
 
-@minecraft.command(name="status", description="server status", guild=discord.Object(id=config.guild_id))
+@server_group.command(name="status", description="server status", guild=discord.Object(id=config.guild_id))
 async def mc_status(ctx: discord.Interaction, ip: str = f"{config.server_ip}:{config.server_port}"):
     if ip.find(":") == -1:
         ip += ":25565"
@@ -133,7 +136,7 @@ async def mc_status(ctx: discord.Interaction, ip: str = f"{config.server_ip}:{co
         except Exception:
             pass
 
-@minecraft.command(name="render", description="render server map", guild=discord.Object(id=config.guild_id))
+@server_group.command(name="render", description="render server map", guild=discord.Object(id=config.guild_id))
 async def mc_render(ctx: discord.Interaction):
     if config.dynmap:
         await ctx.response.send_message(f'http://{config.server_ip}:{config.dynmap_port}')
@@ -169,7 +172,7 @@ async def on_message(message: discord.Message):
                 await message.add_reaction("💀")
 
 async def console_reader(line):
-    global started, panel, start_button_state, stop_button_state, panel_message, console_output, global_interaction, stop_already_called
+    global started, panel, start_button_state, stop_button_state, panel_message, console_output, global_interaction, stop_already_called, nickname
     chat_channel = client.get_channel(config.chat_channel_id)
     console_channel = client.get_channel(config.console_channel_id)
     file = io.open(config.console, mode="r", encoding="utf-8")
@@ -196,7 +199,8 @@ async def console_reader(line):
                 await panel_message.edit(view=view)
             break
         if console_output:
-            nickname = re.search(r"<(.+?)>", line_list[line])
+            if re.match(r"\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9] INFO\]: <(.+?)>", line_list[line]):
+                nickname = re.search(r"<(.+?)>", line_list[line])
             if nickname is not None:
                 await chat_channel.send(f"**{nickname.group(1)}**:{line_list[line][line_list[line].find('>') + 1:]}")
             else:
@@ -204,5 +208,39 @@ async def console_reader(line):
         line += 1
     await asyncio.sleep(0)
     return line
+
+find = client.create_group(name="find", guild=discord.Object(id=config.guild_id))
+
+@find.command(name="ip", description="find ip", guild=discord.Object(id=config.guild_id))
+async def ip(ctx: discord.Interaction):
+    await ctx.response.send_message("in developing")
+
+@find.command(name="ngrok", description="find ngrok", guild=discord.Object(id=config.guild_id))
+async def ngrok(ctx: discord.Interaction, first_index: int = 0, last_index: int = 7, first_port: int = 10000, last_port: int = 20000):
+    await ctx.response.send_message(f"search started from {first_index}.tcp.eu.ngrok.io:{first_port} to {last_index}.tcp.eu.ngrok.io:{last_port}")
+    await find_ngrok(ctx, first_index, last_index, first_port, last_port)
+
+async def find_ngrok(ctx: discord.Interaction, first_index, last_index, first_port, last_port):
+    global find_stop
+    for i in range(first_index, last_index):
+        for port in range(first_port, last_port):
+            await asyncio.sleep(0)
+            if find_stop is True:
+                find_stop = False
+                return
+            server = JavaServer.lookup(f"{i}.tcp.eu.ngrok.io:{port}", timeout=0.05)
+            try:
+                status = server.status()
+                serverinfo = f"ip: {i}.tcp.eu.ngrok.io:{port} | players count: {status.players.online} | version: {status.version.name} | description: {status.description}"
+                await ctx.channel.send(serverinfo)
+            except Exception:
+                pass
+    await ctx.channel.send("search finished")
+
+@find.command(name="stop", description="stop search", guild=discord.Object(id=config.guild_id))
+async def ngrok(ctx: discord.Interaction):
+    global find_stop
+    find_stop = True
+    await ctx.response.send_message("search stopped")
 
 client.run(config.token)
